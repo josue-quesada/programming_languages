48d3fc12d6de1ff7cfbbd185b59ac05aba09a8e8
Josué Quesada <josuequesadaxd@gmail.com>
2023-08-24 11:05:40 -0600

    updated fourth exercise

--- Imperative/Practice 1/Exercise 4/main.go
+++ Imperative/Practice 1/Exercise 4/main.go
@@ -8,3 +8,33 @@ type shoe struct {
 }
 
 type shoes []shoe
+
+var lshoes shoes
+
+func (s *shoes) addShoe(brand string, price int, size int, stock int) {
+	var shoe = s.searchShoe(brand)
+	if shoe != nil {
+		for i := 0; i < len(*s); i++ {
+			if (*s)[i].size == size {
+				(*s)[i].stock += stock
+				if (*s)[i].price != price {
+					(*s)[i].price = price
+				}
+				return
+			}
+		}
+	}
+	*s = append(*s, shoe{brand: brand, price: price, size: size, stock: stock})
+}
+
+func (s *shoes) searchShoe(brand string) *shoe {
+	var result *shoe = nil
+	var i int
+	for i = 0; i < len(*s); i++ {
+		if (*s)[i].brand == brand {
+			result = &((*s)[i])
+			break
+		}
+	}
+	return result
+}
